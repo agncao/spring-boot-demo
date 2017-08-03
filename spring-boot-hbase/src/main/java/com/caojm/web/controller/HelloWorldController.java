e9b97e107c81f3b958f67fc66e103414c872b48a
agncao <agncao@163.com>
2017-08-03 18:12:06 +0800

    项目初始化

---
+++ spring-boot-hbase/src/main/java/com/caojm/web/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.caojm.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author <a href="mailto:caojianmin@jd.com">caojianmin</a>
+ * @create 2017/08/03 16:29
+ */
+@Controller
+@RequestMapping("hello")
+public class HelloWorldController {
+}
