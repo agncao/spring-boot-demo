c2102528f8f1e9c3c126180e3619e4a4948157aa
agncao <agncao@163.com>
2017-08-06 17:41:29 +0800

    spring-boot-thyeleaf

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 一个简单的hello world web小程序，简单的示范了：  
 1. 多模块项目中，当spring-boot程序有自己的母pom时，不用继承Spring Boot的提供的父工程spring-boot-starter-parent
 2. 如何配置thymeleaf;
+3. 静态文件和模板文件均不放在maven模板创建的resources目录之下，而是放在自定义文件夹webapp之下，并将此文件夹设置为资源文件路径;
+4. spring-boot如何整合log4j2;
+## spring-boot-thymeleaf
---
+++ spring-boot-thymeleaf/src/main/java/com/caojm/Application.java
@@ -0,0 +1,21 @@
+package com.caojm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+/**
+ * 程序启动入口
+ *
+ * @author :<a href="mailto:yingjiezhang@ebnew.com">章英杰</a>
+ * @date :2017-02-24 16:05:27
+ */
+
+/** 扫描 路径 scanBasePackages com.caojm 为当前业务基包 */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer{
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);  //运行WEB
+    }
+
+}
