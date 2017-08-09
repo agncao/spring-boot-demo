3b9bbea6bed198e2089119676936414dfa26d5d9
agncao <agncao@163.com>
2017-08-09 11:42:47 +0800

    springcloud注册中心单机版

--- springcloud-eureka/src/main/java/com/caojm/sc/CloudEurekaApplication.java
+++ springcloud-eureka/src/main/java/com/caojm/sc/CloudEurekaApplication.java
@@ -3,15 +3,17 @@ package com.caojm.sc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 /**
- * 程序启动入口
- * 相关源码解读：http://www.jianshu.com/p/cb5cb5937686
+ * <p>启动主类</p>
+ * <p>参考：</p>
+ * <p>http://www.jianshu.com/p/2ec09d0f947a ,http://www.jianshu.com/p/11c99dd04040 <p/>
+ * <p>https://github.com/JeffLi1993/springcloud-learning-example</p>
  * @author :曹建明
  * @date :2017-02-24 16:05:27
  */
-
-/** 扫描 路径 scanBasePackages com.caojm 为当前业务基包 */
 @SpringBootApplication
+@EnableEurekaServer
 public class CloudEurekaApplication extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
